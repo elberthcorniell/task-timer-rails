aa4e274e90a703e18ddf682b003d0e30faa57fc6
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-24 18:05:09 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  resources :users, only: %i[new create show destroy login]
+  
+  # get '/login' => 'users#new'
+  post '/login' => 'users#login'
+  # delete '/logout' => 'users#destroy'
+  # get '/app/' => 'groups#index'
+  # get '/tasks/general' => 'tasks#general'
+  # get '/tasks/done' => 'tasks#done'
+  # patch '/tasks/:id/toggle' => 'tasks#toggle'
 end
