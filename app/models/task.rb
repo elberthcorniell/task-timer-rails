5c76d62b0e79a5b6117691a997562f1f596fdabf
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-24 13:29:12 -0400

    Added models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+    validates :name, presence: true, length: { in: 4..64 }
+    validates :description, length: { in: 4..128 }
+
+    belongs_to :user
+    has_many :timers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { in: 4..64 }
+
+    has_many :tasks
 end
