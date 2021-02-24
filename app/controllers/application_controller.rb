e9bbf0c261fe9ce5e936577ce66003efc9517ec2
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-24 18:04:30 -0400

    Adding current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    def current_user
+        return nil if session[:user_id].nil?
+    
+        begin
+          User.find(session[:user_id])
+        rescue StandardError
+          nil
+        end
+      end
 end
