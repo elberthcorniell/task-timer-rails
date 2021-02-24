52388bd988830fa44e7526129cfcfe288e5da790
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-24 13:29:02 -0400

    Create db migration

--- db/migrate/20210224170711_create_tasks.rb
+++ db/migrate/20210224170711_create_tasks.rb
@@ -1,6 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
+      t.references :user, foreign_key: true
+      t.string :name, null: false
+      t.string :description
 
       t.timestamps
     end
--- db/migrate/20210224170727_create_timers.rb
+++ db/migrate/20210224170727_create_timers.rb
@@ -1,7 +1,9 @@
 class CreateTimers < ActiveRecord::Migration[6.0]
   def change
     create_table :timers do |t|
-
+      t.references :task, foreign_key: true
+      t.timestamp :started_at, null: false
+      t.timestamp :ended_at, null: true
       t.timestamps
     end
   end
