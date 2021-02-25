d70e41fe31fcf5ad7103f10b09be3ccd50083e16
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-25 17:32:35 -0400

    Added cors policies

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,18 @@ module TaskTimerRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    TaskTimerRails::Application.config.session_store :cookie_store, key: '_session_key', expire_after: 14.days
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins 'localhost:3001', 'localhost:3000' , 'http://127.0.0.1:3000', 'http://127.0.0.1:3001'
+         resource '*', 
+          headers: :any,
+          methods: [:get, :post, :options, :delete],
+          credentials: true
+       end
+    end# the framework and any gems in your application.
+
   end
 end
