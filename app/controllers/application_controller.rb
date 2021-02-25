7682b56eaa8852d53e713684cb219d94d1af44db
Elbert Corniell <elberthcorniell@gmail.com>
2021-02-25 17:32:49 -0400

    Added jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require 'jwt'
+
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+    @@jwt_password = '1234'
     def current_user
         return nil if session[:user_id].nil?
     
@@ -8,4 +13,26 @@ class ApplicationController < ActionController::API
           nil
         end
       end
+
+      def create_jwt(user)
+        payload = {
+          username: user.username,
+          id: user.id
+        }
+        JWT.encode payload, @@jwt_password , 'none'
+      end
+
+      def decode_jwt(token)
+        JWT.decode token, @@jwt_password , false
+      end
+
+      def set_logged_in
+        token = request.headers['Authorization'];
+        if token.nil?
+          session[:user_id] = nil;
+        else
+          session[:user_id] = decode_jwt(token)[0]["id"];
+        end  
+      end
+
 end
